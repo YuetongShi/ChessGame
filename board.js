dda30bbce642947e3bc4ade70c79474b6762e37b
Yuetong Shi <yuetong.shi@mail.mcgill.ca>
2025-05-07 16:42:46 -0400

    an empty chessboard

--- board.js
+++ board.js
@@ -8,6 +8,38 @@ function createBoard(){
         for (let col = 0; col <= 9; col++){
             const cell = document.createElement('div')
             cell.classList.add('cell');
+
+            // bottom left corner
+            if (row === 9 && col === 0){
+                cell.classList.add('label');
+            }
+            // upper row and right most column
+            else if (row === 0 || col === 9){
+                cell.classList.add('label');
+            }
+            //letters
+            else if (row === 9){
+                cell.classList.add('label');
+                cell.textContent = letters[col];
+            }
+            // numbers
+            else if (col === 0){
+                cell.classList.add('label');
+                const index = 9 - row;
+                cell.textContent = index.toString();
+            }
+            // checker board
+            else{
+                const isWhite = (row + col) % 2 === 0;
+                if (isWhite)
+                    cell.classList.add('white')
+                else
+                    cell.classList.add('black')
+
+                cell.dataset.col = letters[col];
+                const index = 9 - row;
+                cell.dataset.row = index.toString();
+            }
             board.appendChild(cell);
         }
     }
