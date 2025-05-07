5bb29cb70425c7b4d70fd47f0b991cd3c396906d
Yuetong Shi <yuetong.shi@mail.mcgill.ca>
2025-05-07 16:23:08 -0400

    initial commit

--- board.js
+++ board.js
@@ -3,5 +3,14 @@ const board = document.getElementById('chessboard');
 function createBoard(){
     board.innerHTML = '';
 
-    const letters = [' ', 'A', 'B', 'C','D','E','F','G','H'];
+    const letters = [' ', 'A', 'B', 'C','D','E','F','G','H',' '];
+    for(let row = 0; row <= 9; row++){
+        for (let col = 0; col <= 9; col++){
+            const cell = document.createElement('div')
+            cell.classList.add('cell');
+            board.appendChild(cell);
+        }
+    }
 }
+
+createBoard()
