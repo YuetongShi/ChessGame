9588f506bb51b7bfa43ed1eaf13e37432be73f46
Yuetong Shi <yuetong.shi@mail.mcgill.ca>
2025-06-03 16:05:51 -0400

    Add isVisible

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 import {chessboard, isValidSquare} from "./chessboard";
-import {Color} from "./piece";
+import {Color, Knight} from "./piece";
 
 export class game{
 
@@ -25,9 +25,10 @@ export class game{
             return false;
         else if (!piece.canMove(destLetter, destNumber))
             return false;
+        else if(!this.isVisible(letter, number , destLetter, destNumber, piece))
+            return false;
         else if (this.chessboard.get(destLetter, destNumber) != null)
             return this.chessboard.get(destLetter, destNumber).color !== piece.color;
-        //still need isVisible here, except knight
         //still need isInCheck here, make sure that the king of the same color is not in check
         return true;
     }
@@ -36,8 +37,43 @@ export class game{
 
     }
 
-    isVisible(letter, number, destLetter, destNumber){
+    isVisible(letter, number, destLetter, destNumber, piece){
+        // make sure that there are no obstacles between two squares
+        if (piece instanceof Knight)
+            return true;
 
+        const letterDifference = Math.abs(letter - destLetter);
+        const numberDifference = Math.abs(number - destNumber);
+
+        if (letterDifference <= 1 && numberDifference <= 1)
+            return true;
+
+        let stepLetter = 0;
+        if (destLetter > letter) {
+            stepLetter = 1;
+        } else if (destLetter < letter) {
+            stepLetter = -1;
+        }
+
+        let stepNumber = 0;
+        if (destNumber > number) {
+            stepNumber = 1;
+        } else if (destNumber < number) {
+            stepNumber = -1;
+        }
+
+        let currentLetter = letter + stepLetter;
+        let currentNumber = number + stepNumber;
+
+        while (currentLetter !== destLetter || currentNumber !== destNumber) {
+            if (this.chessboard.get(currentLetter, currentNumber) !== null) {
+                return false;
+            }
+            currentLetter += stepLetter;
+            currentNumber += stepNumber;
+        }
+
+        return true;
     }
 
     moveTo(letter, number, destLetter, destNumber){
